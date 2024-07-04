5f26a37713aa866e858ed2a9992449178d65b56e
[Mariam-Aamir] <[ummeareesh691@gmail.com]>
2024-07-04 13:58:21 +0500

    third assignment commit

---
+++ main.js
@@ -0,0 +1,11 @@
+"use strict";
+//Assingment 03
+Object.defineProperty(exports, "__esModule", { value: true });
+//storing a personName in variable
+let personName = "Mariam Aamir";
+//printing a personName in lowerCase
+console.log("LowerCase", personName.toLowerCase());
+//printing a personName in UpperCase
+console.log("UpperCase", personName.toUpperCase());
+//printing a personName in TitleCase
+console.log("TitleCase", personName.charAt(0).toUpperCase() + personName.slice(1).toLowerCase());
---
+++ main.ts
@@ -0,0 +1,19 @@
+//Assingment 03
+
+//storing a personName in variable
+
+let personName: string = "Mariam Aamir";
+
+//printing a personName in lowerCase
+
+console.log("LowerCase", personName.toLowerCase());
+
+//printing a personName in UpperCase
+
+console.log("UpperCase", personName.toUpperCase());
+
+//printing a personName in TitleCase
+
+console.log("TitleCase", personName.charAt(0).toUpperCase() + personName.slice(1).toLowerCase());
+
+
